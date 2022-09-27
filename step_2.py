93007cdb58f95d156a194476152142fdb523d42b
sergey shatilov <sergeyshatilov3@gmail.com>
2022-09-27 18:55:55 +0900

    Changed README.md

--- step_2.py
+++ step_2.py
@@ -5,3 +5,12 @@ fred = f_money
 oleg = o_money
 john = j_money
 print(john + oleg - fred)
+
+
+
+
+found_coins = 31
+stolen_coins = 4
+magic_coins = 5
+print(found_coins + magic_coins * 365 - stolen_coins * 52 / 2)
+
