c56c436ee0b0a3a924c6854a9ef36f68911973d4
serge <sergeyshatilov3@gmail.com>
2022-11-04 17:18:24 +0900

    Решение упражнений

---
+++ step_4/3.py
@@ -0,0 +1,23 @@
+# нарисовать рамку без углов
+import turtle
+t = turtle.Pen()
+t.forward(100)
+t.up()
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.down()
+t.forward(100)
+t.up()
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.down()
+t.forward(100)
+t.up()
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.down()
+t.forward(100)
+
